aabe3044c776a9821430efc5b2b8996be55410b4
Alex Karavil <alex@karavil.io>
2020-03-18 18:42:35 -0400

    Fetching works, project complete (MVP)

--- src/components/DotaMatches.js
+++ src/components/DotaMatches.js
@@ -1,16 +1,32 @@
 import React, { useEffect } from "react";
 
+import MatchCard from "./MatchCard";
+
 import { getProMatches } from "../actions/dotaActions";
 import { useDispatch, useSelector } from "react-redux";
 
 const DotaMatches = () => {
    const dispatch = useDispatch();
+   const proMatches = useSelector(state => state.proMatches);
 
    useEffect(() => {
       dispatch(getProMatches());
    }, []);
 
-   return <div>test</div>;
+   const MatchCards = proMatches.matches.map(match => (
+      <MatchCard match={match} />
+   ));
+
+   return (
+      <>
+         {proMatches.error ? <h1>Error loading matches. :(</h1> : <></>}
+         {proMatches.loading && !proMatches.error ? (
+            <h1>Matches loading...</h1>
+         ) : (
+            <>{MatchCards}</>
+         )}
+      </>
+   );
 };
 
 export default DotaMatches;
